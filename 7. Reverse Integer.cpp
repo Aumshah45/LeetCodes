1dcdd48daa604c76a2997ab66a04464b6f326b80
Aumshah45 <85445481+Aumshah45@users.noreply.github.com>
2023-06-09 20:23:11 +0530

    Create 7. Reverse Integer.cpp

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,13 @@
+int reverse(int x){
+   int rem,sum=0;
+   while(x!=0){
+       rem=x%10;
+       if((sum>INT_MAX/10) || (sum<INT_MIN/10)){
+           return 0;
+       }
+       sum=sum*10+rem;
+       x=x/10;
+       
+   }
+   return sum;
+}
